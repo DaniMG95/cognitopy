41a2608a562aae35f0e2fb7f472365e998dd8e89
Daniel Muñoz Gonzalez <dani16595@gmail.com>
2023-06-24 21:44:53 +0200

    [[feat]]: update readme and example

--- example/example.py
+++ example/example.py
@@ -1,5 +1,6 @@
 from cognitopy import CognitoPy
 import os
+from cognitopy.enums import MessageAction, DesiredDelivery
 
 # Instantiate CognitoPy object
 
@@ -12,7 +13,10 @@ os.environ["AWS_ACCESS_KEY_ID"] = "XXXXXXXXXXXXXXXXXXXXXXXX"
 os.environ["AWS_SECRET_ACCESS_KEY"] = "XXXXXXXXXXXXXXXXXXXXXXXX"
 
 cognitopy = CognitoPy(
-    userpool_id=COGNITO_USERPOOL_ID, client_id=COGNITO_APP_CLIENT_ID, client_secret=COGNITO_APP_CLIENTE_SECRET
+    userpool_id=COGNITO_USERPOOL_ID,
+    client_id=COGNITO_APP_CLIENT_ID,
+    client_secret=COGNITO_APP_CLIENTE_SECRET,
+    secret_hash=True,
 )
 
 # Register a new user using context
@@ -25,7 +29,7 @@ with CognitoPy(
 
 # Confirm user registration
 
-cognito.confirm_sing_up(username="XXXXX@mail.to", confirmation_code="820850")
+cognito.confirm_register(username="XXXXX@mail.to", confirmation_code="820850")
 
 # Resend confirm code
 
@@ -87,3 +91,70 @@ cognito.admin_add_user_to_group(username="XXXXX@mail.to", group_name="test_group
 # Admin remove user from group
 
 cognito.admin_remove_user_from_group(username="XXXXX@mail.to", group_name="test_group")
+
+# Admin confirm user registration
+
+cognito.admin_confirm_register(username="XXXXX@mail.to")
+
+# Admin create user
+
+cognito.admin_create_user(
+    username="XXXXX@mail.to",
+    force_alias=True,
+    user_attributes={},
+    message_action=MessageAction.SUPPRESS,
+    desired_delivery=[DesiredDelivery.EMAIL],
+)
+
+cognito.admin_create_user(
+    username="XXXXX@mail.to",
+    force_alias=True,
+    user_attributes={},
+    message_action=MessageAction.RESEND,
+    desired_delivery=[DesiredDelivery.EMAIL, DesiredDelivery.SMS],
+    temporary_password="XXXXXXX",
+)
+
+# Admin disable user
+
+cognito.admin_disable_user(username="XXXXX@mail.to")
+
+# Admin enable user
+
+cognito.admin_enable_user(username="XXXXX@mail.to")
+
+# Admin get user
+
+data_user = cognito.admin_get_user(username="XXXXX@mail.to")
+print(data_user)
+
+# Admin login
+
+tokens = cognito.admin_login(username="XXXXX@mail.to", password="XXXXXXX")
+print(tokens)
+
+# Admin renew access token
+
+token = cognito.admin_renew_access_token(access_token="XXXXX", refresh_token="XXXXXXX")
+print(token)
+
+# Admin list groups for user
+
+groups = cognito.admin_list_groups_for_user(username="XXXXX@mail.to", limit=10)
+print(groups)
+groups = cognito.admin_list_groups_for_user(username="XXXXX@mail.to", limit=10, next_token=groups["NextToken"])
+print(groups)
+
+# Admin reset password
+
+cognito.admin_reset_password(username="XXXXX@mail.to")
+
+# Resolve challenge sms mfa
+
+tokens = cognito.resolve_challenge_challenge_sms_mfa(username="XXXXX@mail.to", session="XXXXXX", sms_mfa_code="XXXXXX")
+print(tokens)
+
+# Resolve challenge new password required
+
+tokens = cognito.resolve_challenge_new_password(username="XXXXX@mail.to", session="XXXXXX", new_password="XXXXXX")
+print(tokens)
